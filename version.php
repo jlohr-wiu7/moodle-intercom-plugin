852634ad7362355adc123fda8bcbdbd272b872c9
jlohr-wiu7 <74620867+jlohr-wiu7@users.noreply.github.com>
2020-12-21 12:12:47 -0500

    Update version.php
    Update version to 0.1.1

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_intercom';
-$plugin->release = '0.1.0';
-$plugin->version = 2020111600;
+$plugin->release = '0.1.1';
+$plugin->version = 2020112000;
 $plugin->requires = 2019111800;
 $plugin->maturity = MATURITY_ALPHA;
